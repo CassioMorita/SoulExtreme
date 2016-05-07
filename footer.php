06fd5cfe133755c33a1dfe447d774fbf1f65d0af
Giuliano Cesar Casline <giulianocasline@gmail.com>
2016-05-07 10:32:05 -0300

    Login

---
+++ footer.php
@@ -0,0 +1,13 @@
+      <!-- Site footer -->
+      <footer class="footer">
+        <p>© <?php echo date("Y"); ?> Create by Brain Desenvolvimento Web - Since 2016 - All Rights Reserved</p>
+      </footer>
+
+    </div> <!-- /container -->
+
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="assets/js/ie10-viewport-bug-workaround.js"></script>
+  
+
+</body></html>
